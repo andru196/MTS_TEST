e67a3131ce226fccfa1b885fd3f24f6a6270dadc
andru196 <andru196@icloud.com>
2020-06-05 15:14:28 +0300

    лишние ссылки

--- MTS_TEST/MainWindow.xaml.cs
+++ MTS_TEST/MainWindow.xaml.cs
@@ -1,25 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.IO;
 using Newtonsoft.Json.Linq;
-using System.Reflection;
 using System.Data.Entity;
-using System.Threading;
-using System.Linq.Expressions;
 
 namespace MTS_TEST
 {
